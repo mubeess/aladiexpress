fc24ff5fd28c6e15adb649cf713b5e15f93d315e
mubeess <mubarak.ibrahim@flexisaf.com>
2023-09-06 11:33:23 +0100

    installed styled-components

--- src/pages/Layouts/HomeLayout.jsx
+++ src/pages/Layouts/HomeLayout.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import NavBar from "../../components/NavBar/NavBar";
+import { Outlet } from "react-router-dom";
 
 export default function HomeLayout() {
   return (
-    <div>
-      <h1 style={{ color: "white" }}>Homeeeeeeeee</h1>
+    <div className="main_layout_container">
+      <Header />
+      <NavBar />
+      <div className="main_layout_body">
+        <Outlet />
+      </div>
     </div>
   );
 }
