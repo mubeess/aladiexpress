d8a6afc1d42fd899879214b125f318cf7c850cd0
Mujtaba Adamu <mujtaba.adamu@flexisaf.com>
2023-09-08 18:27:17 +0100

    Card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 
 export default function Home() {
   return (
     <div>
       <h1 style={{ color: "white" }}>Homeeeeeeeee</h1>
+      <Card />
     </div>
   );
 }
