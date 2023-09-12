9f192c137b82b96d40e7cc26e49f410b1531829c
Mujtaba Adamu <mujtaba.adamu@flexisaf.com>
2023-09-12 18:32:38 +0100

    add responsive

--- src/routes/IndexRoute.jsx
+++ src/routes/IndexRoute.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import HomeLayout from "../pages/Layouts/HomeLayout";
 import Product from "../pages/Product";
+import ProductDetails from "../pages/PriductDetails";
 
 export default function IndexRoute() {
   return (
     <Routes>
       <Route path="/" element={<HomeLayout />}>
         <Route index element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<ProductDetails />} />
       </Route>
     </Routes>
   );
